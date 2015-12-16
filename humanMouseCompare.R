5b18f9a85c1d5d8f964a0c2d18e70f49a0337eda
kanishk <kani.asthana@gmail.com>
2015-12-15 22:39:57 -0800

    Getting common(orthologous) mouse and human genes in graphs

--- humanMouseCompare.R
+++ humanMouseCompare.R
@@ -10,3 +10,15 @@ geneIntersect=intersect(allMouseProteinCodingGeneNames,mouseGeneNamesInGraph)
 #It seems like not all genes are protein coding!~87% protein Coding Genes. Its fine
 print("Percentage of Protein Coding Genes:")
 print(100*length(geneIntersect)/length(mouseGeneNamesInGraph))
+
+humanGeneNamesInGraph=read.csv("geneNamesHuman.csv", header=FALSE, stringsAsFactors = FALSE)
+humanGeneNamesInGraph=unlist(humanGeneNamesInGraph)
+
+#By convention human gene names are named in all Capital Letters and their mouse counterparts with only the
+#first letter capitalized. See this link http://www.ncbi.nlm.nih.gov/books/NBK21060/#A5950
+#To get a list of genes that is common to both mouse and human we will convert all mouse gene names to uppercase
+# and then take the intersection of human and mouse gene names in the graph
+
+mouseGeneNamesInGraphUPPERCASE=sapply(mouseGeneNamesInGraph,toupper)
+#Taking intersection: 13261 common human and mouse genes. Nice!
+commonMouseAndHumanGenes=intersect(humanGeneNamesInGraph,mouseGeneNamesInGraphUPPERCASE)
