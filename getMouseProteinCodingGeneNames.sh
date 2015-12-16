319c9394d19bd74ed5cb64f35d54e4d79b2fb3f5
kanishk <kani.asthana@gmail.com>
2015-12-15 22:03:28 -0800

    Changing bash file so it shows the number of genes

--- getMouseProteinCodingGeneNames.sh
+++ getMouseProteinCodingGeneNames.sh
@@ -4,7 +4,9 @@
 awk '{if($3=="gene" && $12=="\"protein_coding\";"){print $0}}' gencode.vM7.annotation.gtf  > allMouseProteinCodingGeneAnnotations.gtf
 
 #Counting number of genes. Is consistent with what is on gencode website
-wc -l allMouseProteinCodingGeneAnnotations.gtf
+wc -l allMouseProteinCodingGeneAnnotations.gtf >file
+cat file
+rm file
 #Getting protein coding mouse gene names and storing them in a csv file
 awk '{print $16}' allMouseProteinCodingGeneAnnotations.gtf > allMouseProteinCodingGeneNames.csv
 
