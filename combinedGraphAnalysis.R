651fe20a12c1911a31f0debd673e53db2e2f21c8
kanishk <kani.asthana@gmail.com>
2015-12-18 00:25:43 -0800

    Updating code to view and plot common edge distribution for human and mouse

--- combinedGraphAnalysis.R
+++ combinedGraphAnalysis.R
@@ -2,7 +2,7 @@
 humanLogicalGraph=read.csv("humanLogicalGraph.csv", header=TRUE)
 mouseLogicalGraph=read.csv("mouseLogicalGraph.csv", header=TRUE)
 
-#Setting Diagnal of Both LogicalGraphs to FALSE. i.e. We are ignoring self edges.
+#Setting Diagonal of Both LogicalGraphs to FALSE. i.e. We are ignoring self edges.
 diag(humanLogicalGraph)=FALSE
 diag(mouseLogicalGraph)=FALSE
 mouseGeneNamesInGraph=read.csv("geneNamesMouse.csv",header=FALSE)
@@ -11,7 +11,7 @@ mouseGeneNamesInGraph=read.csv("geneNamesMouse.csv",header=FALSE)
 mouseGeneNamesInGraph=unlist(mouseGeneNamesInGraph)
 humanGeneNamesInGraph=read.csv("geneNamesHuman.csv", header=FALSE)
 #Unlisting to perform set operations
-humanGeneNamesInGraph=unlist(humanGeneNamesInGraph)
+humanGeneNamesInGraph=as.factor(unlist(humanGeneNamesInGraph))
 
 #By convention human gene names are named in all Capital Letters and their mouse counterparts with only the
 #first letter capitalized. See this link http://www.ncbi.nlm.nih.gov/books/NBK21060/#A5950
@@ -39,7 +39,7 @@ print(dim(mouseLogicalGraphIntersectWithHuman))
 vectorForNumberOfEdgesHuman=apply(humanLogicalGraphIntersectWithMouse,2,sum)
 vectorForNumberOfEdgesMouse=apply(mouseLogicalGraphIntersectWithHuman,2,sum)
 
-mHist=hist(vectorForNumberOfEdgesMouse,100)
+mHist=hist(vectorForNumberOfEdgesMouse,100,main="Histogram of Edge distribution of Mouse Graph Intersect", xlab="Number of Edges", ylab="Frequency of Nodes")
 hHist=hist(vectorForNumberOfEdgesHuman,100,main="Histogram of Edge distribution of Human Graph Intersect", xlab="Number of Edges", ylab="Frequency of Nodes")
 
 pdf("LogPlotForHistogramsEdgeDistrubutionMouseAndHumanGraphIntersect.pdf")
@@ -47,3 +47,18 @@ plot(mHist$breaks[2:length(mHist$breaks)],log(mHist$counts), type='S', xlab="Num
 plot(hHist$breaks[2:length(hHist$breaks)],log(hHist$counts), type='S', xlab="Number of Edges", ylab="Log of Frequency of Nodes", main="Log plot of Histogram of Edge distribution of Human Graph Intersect")
 dev.off()
 #Getting rid of the NA values fixed the problem. But the graph is still very sparse.
+
+#Creating percentage vector
+numberOfCommonEdgesVector=sapply(1:length(commonMouseAndHumanGenes),function(i){
+  humanEdgesInGraph=humanGeneNamesInGraph[humanLogicalGraphIntersectWithMouse[,i]]
+  mouseEdgesInGraph=mouseGeneNamesInGraphUPPERCASE[mouseLogicalGraphIntersectWithHuman[,i]]
+  commonEdgesInGraphs=intersect(humanEdgesInGraph,mouseEdgesInGraph)
+  return(length(commonEdgesInGraphs))
+})
+
+write.table(numberOfCommonEdgesVector,"commonEdgesVector.csv",sep=",",row.names=FALSE, col.names=FALSE, quote=FALSE)
+
+commonHist=hist(numberOfCommonEdgesVector,50, main="Histogram of Common Edges Distribution for Human and Mouse Graphs", xlab="Number of Edges", ylab="Frequency of Nodes")
+pdf("LogPlotForNumberOfCommonEdgesIntheHumanAndMouseGraphs.pdf")
+plot(commonHist$breaks[2:length(commonHist$breaks)], log(commonHist$counts), type='S', xlab="Number of Edges", ylab="Log of Frequency of Nodes", main="Log plot of Histogram of Common Edges distribution for Human and Mouse Graphs")
+dev.off()
