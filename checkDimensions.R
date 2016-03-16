1e2f9471ec27a00a15aa0ff8481c4923d79e2615
kanishk <kani.asthana@gmail.com>
2016-03-16 03:35:14 -0700

    Adding code to check dimensions of generated matrices

---
+++ checkDimensions.R
@@ -0,0 +1,4 @@
+humanLogicalGraph=read.csv("humanLogicalGraph.csv", header=TRUE)
+print(dim(humanLogicalGraph))
+mouseLogicalGraph=read.csv("mouseLogicalGraph.csv", header=TRUE)
+print(dim(mouseLogicalGraph))
