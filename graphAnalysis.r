8c1c355a5207ff71fa8d49e881c4785d12555e72
kanishk <kani.asthana@gmail.com>
2016-03-18 12:23:31 -0700

    Adding code to get cliques from the graph.

--- graphAnalysis.r
+++ graphAnalysis.r
@@ -1,6 +1,17 @@
 
 humanGraph=read.csv("graph_outputHumanAfterBugFix.csv",header=TRUE)
+#Labeling both columns and rows
+rownames(humanGraph)=colnames(humanGraph)
+#Temporarily taking only a subset of the graphs for writting the program
+#humanGraph=humanGraph[1:1000,1:1000]
+gc()#Garbage Collection to free up memory
 mouseGraph=read.csv("graph_outputMouseAfterBugFix.csv",header=TRUE)
+#Labeling both column and rows
+rownames(mouseGraph)=colnames(mouseGraph)
+#Temporarily taking only a subset of the graphs for writting the program
+#mouseGraph=mouseGraph[1:1000,1:1000]
+gc()#Garbage Collection to free up memory
+
 humanGraphLogical=(humanGraph!=0)
 mouseGraphLogical=(mouseGraph!=0)
 #Each cell of this graph will return a True value only if that edge is present in both graphs
@@ -25,3 +36,9 @@ print("Total Number of Edges in Intersect graph:")
 print(sum(intersectGraphLogical))
 
 library('igraph')
+
+intersectIGraph=graph.adjacency(intersectGraphLogical, mode = c("undirected"), weighted = NULL, diag=FALSE)
+
+intersectGraphCliques=max_cliques(intersectIGraph,min=2,file="intersectGraphCliques.txt")
+
+
